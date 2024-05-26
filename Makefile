c0eb1719abd542ea5bafb86ee4601cbf98de90e7
Matteo Maggiolo <maggiolo.matteo@gmail.com>
2024-05-26 18:49:33 +0100

    Add Boost dependency and Makefile

---
+++ Makefile
@@ -0,0 +1,17 @@
+BUILD_TYPE ?= Release
+
+.PHONY: all install build
+
+all: install build
+	@
+
+install:
+	@ conan install . --build=missing -sbuild_type=${BUILD_TYPE}
+
+build:
+	@ mkdir -p build
+	@ cd build && cmake .. -G"Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=${BUILD_TYPE}/generators/conan_toolchain.cmake  -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_BUILD_TYPE=${BUILD_TYPE}
+	@ cd build && make
+
+clean:
+	@ find build ! -name .gitignore -depth 1 | xargs rm -rf
--- conanfile.py
+++ conanfile.py
@@ -1,10 +1,19 @@
 from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
 
 class ConanPackage(ConanFile):
     name = "network-monitor"
     version = "0.1.0"
     
-    generators = ["CMakeToolchain", "CMakeDeps"]
-    settings = "build_type"
+    generators = ("CMakeToolchain", "CMakeDeps")
+    settings = ("os", "compiler", "build_type", "arch")
     
-    requires = []
+    def requirements(self):
+        self.requires("boost/1.85.0")
+    
+    def configure(self):
+        self.options["boost/*"].shared = False
+
+    def layout(self):
+        cmake_layout(self)
