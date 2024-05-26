07449306fc3eb028e3d6e6d96342c94ccb6cdc27
Matteo Maggiolo <maggiolo.matteo@gmail.com>
2024-05-26 23:11:29 +0100

    Convert code to use async API

--- src/main.cpp
+++ src/main.cpp
@@ -23,64 +23,88 @@ void Log(std::string message)
 
 int main()
 {
-    std::string host = "ltnm.learncppthroughprojects.com";
-    std::string port = "80";
-    std::string path = "/echo";
-    std::string message = "Hello World Wide Web!";
+    // Initialize constants
+    const std::string host = "ltnm.learncppthroughprojects.com";
+    const std::string port = "80";
+    const std::string path = "/echo";
+    const std::string message = "Hello World Wide Web!";
 
     // Always start with an I/O context object.
     boost::asio::io_context ioc {};
 
-    // Under the hood, socket.connect uses I/O context to talk to the socket
-    // and get a response back. The response is saved in ec.
+    // Initialize error container
     boost::system::error_code ec {};
+
+    // Initialize the TCP resolver
     tcp::resolver resolver {ioc};
 
-    auto resolverIt {resolver.resolve(host, port, ec)};
-
-    if (ec) {
-        LogErr(ec);
-        return -1;
-    }
-
-    // Create a TCP socket
-    tcp::socket socket {ioc};
-    // Connect the socket to the IP address of the server
-    socket.connect(*resolverIt, ec);
-
-    // If failed to connect, error out
-    if (ec) {
-        LogErr(ec);
-        return -1;
-    }
-
-    // Create a websocket to use the TCP stream
-    boost::beast::websocket::stream<boost::beast::tcp_stream> ws {std::move(socket)};
-    // Perform handshake
-    ws.handshake(host, path, ec);
-
-    if (ec) {
-        LogErr(ec);
-        return -1;
-    }
-
-    // Write message
-    boost::asio::const_buffer wbuffer {message.c_str(), message.size()};
-    ws.write(wbuffer, ec);
-
-    if (ec) {
-        LogErr(ec);
-        return -1;
-    }
-
-    // Read message
+    // Create a websocket stream object
+    boost::beast::websocket::stream<boost::beast::tcp_stream> ws {ioc};
+    
+    // Initialize buffer to store the response from the API
     boost::beast::flat_buffer resp;
-    ws.read(resp);
-    std::string response = boost::beast::buffers_to_string(resp.data());
 
-    // Relay the message
-    Log("Message received:");
-    Log(response);
+    // Define the chain of callbacks in reversed order (painful)
+    auto onRead = [&resp](boost::system::error_code ec, std::size_t transferred) {
+        if (ec) {
+            Log("Failed async_read");
+            LogErr(ec);
+            return;
+        }
+
+        std::string response = boost::beast::buffers_to_string(resp.data());
+
+        Log("Received message");
+        Log(response);
+
+    };
+
+    auto onWrite = [&ws, &resp, &onRead](boost::system::error_code ec, std::size_t transferred) {
+        if (ec) {
+            Log("Failed async_write");
+            LogErr(ec);
+            return;
+        }
+
+        ws.async_read(resp, onRead);
+    };
+
+    auto onHandshake = [&ws, &message, &onWrite](boost::system::error_code ec) {
+        if (ec) {
+            Log("Failed async_handshake");
+            LogErr(ec);
+            return;
+        }
+
+        boost::asio::const_buffer wbuffer {message.c_str(), message.size()};
+        ws.async_write(wbuffer, onWrite);
+    };
+
+    auto onConnect = [&ws, &host, &path, &onHandshake](boost::system::error_code ec) {
+        if (ec) {
+            Log("Failed async_connect");
+            LogErr(ec);
+            return;
+        }
+
+        ws.async_handshake(host, path, onHandshake);
+    };
+
+    auto onResolve = [&ws, &onConnect](boost::system::error_code ec, tcp::resolver::results_type resResults) {
+        if (ec) {
+            Log("Failed async_resolve");
+            LogErr(ec);
+            return;
+        }
+
+        ws.next_layer().async_connect(*resResults, onConnect);
+    };
+
+    // Start the chain of resolvers with the first async method
+    resolver.async_resolve(host, port, onResolve);
+
+    // Start the loop in the io context
+    ioc.run();
 
     return 0;
 }
