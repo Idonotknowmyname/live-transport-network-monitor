3ab6e71c6e08013ef64058c5afd79e7c57184d65
Matteo Maggiolo <maggiolo.matteo@gmail.com>
2024-05-27 17:32:39 +0100

    Add basic logging functionality

---
+++ src/logging.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <boost/asio.hpp>
+#include <boost/system/error_code.hpp>
+#include <boost/beast.hpp>
+
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+// #define LOG_LEVEL_WIDTH 14
+
+namespace mm {
+
+
+    void LogErr(boost::system::error_code ec)
+    {
+        std::cerr << "[" << std::setw(14) << std::this_thread::get_id() << "] "
+                << (ec ? "Error: " : "No error: OK")
+                << (ec ? ec.message() : "")
+                << std::endl;
+    }
+
+    template<typename StreamType, typename T, typename... Types>
+    void Log(StreamType& stream, T firstArg, Types... args)
+    {
+        stream << firstArg << " ";
+        if constexpr(sizeof...(args) > 0) {
+            Log(stream, args...);
+        } else {
+            stream << std::endl;
+        }
+    }
+
+    template<typename... Types>
+    void LogInfo(Types... args) {
+        Log(std::cout, args...);
+    }
+}
